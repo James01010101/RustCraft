fa3e0cace6dbf6fd18e82c1907d90fd43e141fc4
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-16 23:51:50 +1030

    Moved all files out into just one crate

--- GameEngine/src/TestFuncs.rs
+++
@@ -1,5 +0,0 @@
-
-// declare functions in this module however i want
-pub fn Add(a: u32, b: u32) -> u32 {
-    return a + b;
-}
--- GameEngine/src/lib.rs
+++
@@ -1,7 +0,0 @@
-
-#![allow(non_snake_case)]
-#![allow(dead_code)]
-
-// declare public modules, create a file for them that is the same name and write whatever i want in them
-pub mod TestFuncs;
-
