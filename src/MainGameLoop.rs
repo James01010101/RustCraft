a74c5892b6a6b35d8ee246700719402b0462dea7
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-18 22:47:37 +1030

    Wrote the new full game loop using minifb

--- src/MainGameLoop.rs
+++ src/MainGameLoop.rs
@@ -1,63 +1,46 @@
 
 use crate::Renderer::*;
-use gl::{Clear, ClearColor};
-use glfw::{Key, Action, Context};
-use libc::c_void;
-use std::mem;
+use crate::Kernels::{Kernels, CreateKernels};
+use crate::GPUKernels::PixelGradient::RunPixelGradientKernel;
+use crate::GPUKernels::PixelShift::RunPixelShiftKernel;
 
-pub fn RunMainGameLoop() -> Result<()> {
+use minifb::{Key};
+use std::mem;
+use std::thread;
+use std::time::Duration;
+
+pub fn RunMainGameLoop() {
 
     // create Renderer and window
     let mut renderer: Renderer = CreateRenderer(800, 600);
-    let c: u32 = 0;
+
+    // create my kernels objects which will compile all my kernels
+    let kernels: Kernels = CreateKernels(&renderer);
+
+    // Blocking write
+    RunPixelGradientKernel(&mut renderer, &kernels);
+    mem::swap(&mut renderer.pixelBuffer1, &mut renderer.pixelBuffer2);
+    
+    let mut frameNumber: u64 = 0;
+    
 
     // Loop until the user closes the window
-    while !renderer.window.should_close() {
+    while renderer.window.is_open() && !renderer.window.is_key_down(Key::Escape) {
+        frameNumber += 1;
+
+        //RunPixelShiftKernel(&mut renderer, &kernels);
         
-
-        // Poll for and process events
-        renderer.glfwObj.poll_events();
-        for (_, newEvent) in glfw::flush_messages(&renderer.events) {
-            handle_window_event(&mut renderer.window, newEvent);
+        // Switch buffers if needed, e.g., based on user input or timing
+        // Example: Switch buffer on spacebar press
+        if renderer.window.is_key_down(Key::Space) {
+            mem::swap(&mut renderer.pixelBuffer1, &mut renderer.pixelBuffer2);
         }
-
-        // do stuff with the events
-
-
-        /* Old render code
-        // Render here
-        unsafe {
-            gl::ClearColor(0.0, 1.0, 0.0, 1.0); // For example, clear the screen to green
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-        // Swap front and back buffers
-        renderer.window.swap_buffers();
-        */
 
         // render the image
-        RenderTexture(&mut renderer);
-    }
+        // always render buffer 1, but swap buffer 1 & 2 as ill be working on 3 and 2 is the newest finished frame
+        //mem::swap(&mut renderer.pixelBuffer1, &mut renderer.pixelBuffer2);
+        renderer.window.update_with_buffer(&renderer.pixelBuffer1, renderer.screenWidth, renderer.screenHeight).unwrap();
 
-    Ok(())
-}
-
-fn RenderTexture(renderer: &mut Renderer) {
-    // TODO: #18 update the texture
-    // swap the buffer 1 and 2
-    mem::swap(&mut renderer.pixelBuff1, &mut renderer.pixelBuff2);
-
-    // render buffer 1
-    unsafe {
-        gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGBA32F as i32, renderer.screenWidth as i32, renderer.screenHeight as i32, 0, gl::RGBA, gl::FLOAT, renderer.pixelBuff1.as_ptr() as *const c_void);
-    }
-    renderer.window.swap_buffers();
-}
-
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true)
-        }
-        _ => {}
-    }
+        //thread::sleep(Duration::from_secs(1));
+    }   
 }
