85c6f398e74dcae1a19858c2d60eea0e00609c52
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-18 22:46:49 +1030

    cleaned up errors for formatting and types

--- src/Kernels.rs
+++ src/Kernels.rs
@@ -1,34 +1,38 @@
 
-use crate::GPUKernels::*;
+use crate::GPUKernels::{PixelGradient, PixelShift};
 use crate::Renderer::{Renderer};
 
 use opencl3::kernel::{Kernel};
 use opencl3::program::{Program};
 
 // this will store the compiled kernels
-struct Kernels {
-    pixelGradientKernel: Kernel,
+pub struct Kernels {
+    pub pixelGradientKernel: Kernel,
+    pub pixelShiftKernel: Kernel,
 }
 
 
 // this will go through and compile all kernels and save them to the Kernels Struct and return the Kernels struct
-pub fn CreateKernels(&renderer: Renderer) -> Kernels {
+pub fn CreateKernels(renderer: &Renderer) -> Kernels {
 
-    let pixelGradient = CompileKernel(&renderer, GPUKernels::pixelGradientKernel, GPUKernels::pixelGradientKernelName);
-
-
+    let pixelGradient = CompileKernel(&renderer, PixelGradient::pixelGradientKernel, PixelGradient::pixelGradientKernelName);
+    let pixelShift = CompileKernel(&renderer, PixelShift::pixelShiftKernel, PixelShift::pixelShiftKernelName);
+    
     let kernels = Kernels {
         pixelGradientKernel: pixelGradient,
+        pixelShiftKernel: pixelShift,
     };
 
     return kernels;
 }
 
 // compile the kernels
-pub fn CompileKernel(&renderer: Renderer, &program: str, &name: str) {
+pub fn CompileKernel(renderer: &Renderer, program: &str, name: &str) -> Kernel {
     println!("Compiling Kernel: {}", name);
+
     let builtProgram = Program::create_and_build_from_source(&renderer.context, program, "")
         .expect("Program::create_and_build_from_source failed");
+
     let kernel = Kernel::create(&builtProgram, name).expect("Kernel::create failed");
 
     return kernel;
