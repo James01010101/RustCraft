cc0839620135e2c582d6c2cff4177fd78a2c87cd
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-19 23:57:44 +1030

    the world struct which will store all chunks

---
+++ src/World.rs
@@ -0,0 +1,16 @@
+use crate Chunk::*;
+use std::collections::HashMap;
+
+
+// this struct will hold all of the Chunks as well as arrays of mobs
+struct World {
+
+    // TODO: #25 Use a hashmap to store currently loaded chunks
+    chunks: HashMap<Chunk>
+}
+
+
+// TODO: #22 Make the create world function
+pub fn CreateWorld() {
+ 
+}
