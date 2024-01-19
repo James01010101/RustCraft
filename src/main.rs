0b47ed9b7fdbb2e4168b30eba4126d94436c9f2e
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-19 23:54:33 +1030

    Added modules Object, World, Settings

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,16 @@
 #![allow(unused_variables)]
 
 // create any modules i need
-pub mod MainGameLoop;
+pub mod MainGameLoop; // where i create the window and renderer and the main loop
 pub mod Renderer;
 pub mod GPUKernels; // where i write all gpu kernels
 pub mod Kernels;    // where i store the compiled kernels
-
+pub mod Objects; // where i create my basic objects like spheres and squares
+pub mod World; // this is where all of the objects in the world are stored
+pub mod Settings; // this is a const settings file for things like chunk size, ray bounces ect.
 
 fn main() {
+
 
     MainGameLoop::RunMainGameLoop();
 }
