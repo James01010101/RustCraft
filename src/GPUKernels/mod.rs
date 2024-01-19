90d0777241c445488d5a1d31c7ce68363ddffacf
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-19 23:56:22 +1030

    Started to add raytrace kernel

--- src/GPUKernels/mod.rs
+++ src/GPUKernels/mod.rs
@@ -2,3 +2,4 @@
 
 pub mod PixelGradient;
 pub mod PixelShift;
+pub mod Raytrace;
--- src/Kernels.rs
+++ src/Kernels.rs
@@ -1,5 +1,5 @@
 
-use crate::GPUKernels::{PixelGradient, PixelShift};
+use crate::GPUKernels::{PixelGradient, PixelShift, Raytrace};
 use crate::Renderer::{Renderer};
 
 use opencl3::kernel::{Kernel};
@@ -9,18 +9,25 @@ use opencl3::program::{Program};
 pub struct Kernels {
     pub pixelGradientKernel: Kernel,
     pub pixelShiftKernel: Kernel,
+    pub raytraceKernel: Kernel,
 }
 
 
 // this will go through and compile all kernels and save them to the Kernels Struct and return the Kernels struct
 pub fn CreateKernels(renderer: &Renderer) -> Kernels {
 
+    // testing kernels
     let pixelGradient = CompileKernel(&renderer, PixelGradient::pixelGradientKernel, PixelGradient::pixelGradientKernelName);
     let pixelShift = CompileKernel(&renderer, PixelShift::pixelShiftKernel, PixelShift::pixelShiftKernelName);
+
+    // main kernels
+    let raytrace = CompileKernel(&renderer, Raytrace::raytraceKernel, Raytrace::raytraceKernelName);
     
+
     let kernels = Kernels {
         pixelGradientKernel: pixelGradient,
         pixelShiftKernel: pixelShift,
+        raytraceKernel: raytrace,
     };
 
     return kernels;
