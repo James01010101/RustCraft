b992cc0365a9628d4c693366fcae6c0ba4a16b21
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-19 23:55:30 +1030

    added Chunk module

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,8 @@ pub mod GPUKernels; // where i write all gpu kernels
 pub mod Kernels;    // where i store the compiled kernels
 pub mod Objects; // where i create my basic objects like spheres and squares
 pub mod World; // this is where all of the objects in the world are stored
-pub mod Settings; // this is a const settings file for things like chunk size, ray bounces ect.
+pub mod Settings; // this is a const settings file for things like chunk size, ray bounces ect.\
+pub mod Chunk; // where the blocks and chunks are stored
 
 fn main() {
 
