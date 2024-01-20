b0c6800c93e09a8ec9ca0455aeb1933ce778c57b
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-20 21:19:43 +1030

    worked on Create tris from blocks. not finished yet

--- src/Objects.rs
+++ src/Objects.rs
@@ -1,4 +1,4 @@
-
+use crate::Settings::*;
 
 
 // a basic 3 int position position struct to store the xyz position of a block
@@ -7,10 +7,11 @@ pub struct Position {
     // i16 max/min = 32,767
     // i32 max/min = 2,147,483,647
 
-    x: i32,
-    y: i32,
-    z: i16,
+    pub x: i32,
+    pub y: i32,
+    pub z: i16,
 }   
+
 
 // what type of block is it
 pub enum BlockType {
@@ -44,28 +45,53 @@ pub struct Block {
     position: Position,
 
     // Should add a vector of tris here that are pre computed, so its one less thing i need to calculate
-    // every time i need to check for collisions
-    // TODO: #27 add vector of tris as faces
-    faces: [Tris, 12],
+    // every time i need to check for collisions, there will be exactly 12
+    faces: [Tris; 12],
 
 }
 
 
 // from the position of a block create all tris
-pub fn CreateTrisFromBlock(pos: &Position) -> Vec<Tris> {
+pub fn CreateTrisFromBlock(pos: &Position) -> [Tris; 12] {
+    // create all verts (share them between edges to save space)
+    // Name (xyz) or (Left/Right, Front/Back, Top/Bottom)
+    
+    let LBB: Position = Position { x: pos.x, y: pos.y, z: pos.z }; // this will be the position passed in
+    let LBT: Position = Position { x: pos.x, y: pos.y, z: pos.z + 1 };
+
+    let LFB: Position = Position { x: pos.x, y: pos.y - 1, z: pos.z };
+    let LFT: Position = Position { x: pos.x, y: pos.y - 1, z: pos.z + 1 };
+
+    let RBB: Position = Position { x: pos.x + 1, y: pos.y, z: pos.z };
+    let RBT: Position = Position { x: pos.x + 1, y: pos.y, z: pos.z + 1 };
+
+    let RFB: Position = Position { x: pos.x + 1, y: pos.y - 1, z: pos.z };
+    let RFT: Position = Position { x: pos.x + 1, y: pos.y - 1, z: pos.z + 1 };
+
+    // now make the tris from all the positions and return it
+    let mut trisArr: [Tris; 12] = todo!();
+
+    return trisArr;
 
 }
 
 
 pub fn CreateNewBlock(blockType: BlockType, dynamic: bool, posX: i32, posY: i32, posZ: i16) -> Block {
     
-    let newPos: Position = Position { x: posX, y: posY, z: posZ };
+    // if the block is dynamic then i create its own Position so i can change it,
+    // other wise it gets a non mutable reference to a large table where all positions created are stored
+
+    let mut newPos: Position = Position { x: posX, y: posY, z: posZ };
+
+    let mut tris: [Tris; 12] = CreateTrisFromBlock(&newPos);
     
-    let newBlock: Block = Block {
+    let mut newBlock: Block = Block {
         blocksType: blockType,
         dynamic: dynamic,
         position: newPos,
+        faces: tris,
     };
 
     return newBlock;
 }
+
