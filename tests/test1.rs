642a6d5cc38eb414ceae2b5b6b8c733b9e54c513
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-26 17:53:08 +1030

    removed some testing tests

--- tests/test1.rs
+++ tests/test1.rs
@@ -1,25 +1,15 @@
 
 #![allow(non_snake_case)]
 
-extern crate GameEngine;
-
-use GameEngine::TestFuncs::{Add};
-
 
 #[test]
 fn Test_Add1() {
-    let result = Add(2, 2);
+    let result = 2 + 2;
     assert_eq!(result, 4);
 }
 
 #[test]
 fn Test_Add2() {
-    let result = Add(4, 7);
-    assert_eq!(result, 11);
-}
-
-#[test]
-fn Test_Add3() {
-    let result = Add(2, 0);
-    assert_eq!(result, 2);
+    let result = 4 + 7;
+    assert_eq!(result, 12, "Testing addition of {} + {}", 4, 7);
 }
