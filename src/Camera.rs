d9eab5d5c173a83b246ee22fe2098027b3fbbb38
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-26 18:49:06 +1030

    Renamed and made public variables

--- src/Camera.rs
+++ src/Camera.rs
@@ -3,32 +3,32 @@ use crate::Objects::Position;
 
 
 pub struct Camera {
-    fov: f32,
-    aspect_ratio: f32,
-    near_plane: f32,
-    far_plane: f32,
+    pub fov: f32,
+    pub aspectRatio: f32,
+    pub nearPlane: f32,
+    pub farPlane: f32,
 
-    cameraPosition: Position,
-    cameraTarget: Position,
+    pub cameraPosition: Position,
+    pub cameraTarget: Position,
 
 }
 
 
 impl Camera {
-    pub fn new(fov: f32, screenWidth: u32, screenHeight: u32) {
-        let aspect_ratio: f32 = screenWidth as f32 / screenHeight as f32;
-        let near_plane: f32 = 0.1;
-        let far_plane: f32 = 100.0;
+    pub fn new(fov: f32, screenWidth: u32, screenHeight: u32) -> Camera {
+        let aspectRatio: f32 = screenWidth as f32 / screenHeight as f32;
+        let nearPlane: f32 = 0.1;
+        let farPlane: f32 = 100.0;
 
         // initial position and target
-        let cameraPosition: Position = Position::new(0.0, 0.0, -5.0);
-        let cameraTarget: Position = Position::new(0.0, 0.0, 0.0);
+        let cameraPosition: Position = Position::new(0, 0, 0);
+        let cameraTarget: Position = Position::new(0, 0, 0);
 
         Camera {
             fov: fov,
-            aspect_ratio,
-            near_plane,
-            far_plane,
+            aspectRatio,
+            nearPlane,
+            farPlane,
 
             cameraPosition,
             cameraTarget,
