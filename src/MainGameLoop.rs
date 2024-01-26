c4c422073fcbbd238b51c7493e62ce7dd9333a33
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-26 23:50:47 +1030

    Now drawing a group of tris at a time

--- src/MainGameLoop.rs
+++ src/MainGameLoop.rs
@@ -1,52 +1,120 @@
 
 use crate::Renderer::*;
-use crate::Kernels::{Kernels, CreateKernels};
-use crate::GPUKernels::PixelGradient::RunPixelGradientKernel;
-use crate::GPUKernels::PixelShift::RunPixelShiftKernel;
+use crate::Kernels::Kernels;
+use crate::GPUKernels;
 use crate::World::*;
 use crate::Chunk::*;
 use crate::Objects::*;
+use crate::GPUData::GPUData;
 
+extern crate gl;
+extern crate glfw;
 
-use minifb::{Key};
+use glfw::Context;
 
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
 use std::mem;
 
+use nalgebra::{Point3, Vector3};
+
+
 pub fn RunMainGameLoop() {
 
     println!("Size of Block: {} bytes", mem::size_of::<Block>());
 
     // create Renderer and window
-    let mut renderer: Renderer = CreateRenderer(1920, 1080);
+    let mut renderer: Renderer = Renderer::new(1920, 1080, 80.0);
 
     // create my kernels objects which will compile all my kernels
-    let kernels: Kernels = CreateKernels(&renderer);
+    //let kernels: Kernels = CreateKernels(&renderer);
 
     // create my world
     let world: World = CreateWorld();
 
+    // create the gpudata (vao, vbo, ebo)
+    let gpuData: GPUData = GPUData::new();
+    
+
+    
+
+    let mut angle: f32 = 0.0; // Current angle of rotation
+    let rotation_speed: f32 = 0.0001; // Speed of rotation
+    let radius: f32 = 2.0; // Distance from the center
     
     // stats before starting
     let mut frameNumber: u64 = 0;
     let windowStartTime = Instant::now();
-
-    // Loop until the user closes the window
-    while renderer.window.is_open() && !renderer.window.is_key_down(Key::Escape) {
+    while !renderer.window.should_close() {
         frameNumber += 1;
 
-        
-        // Switch buffers if needed, e.g., based on user input or timing
-        // Example: Switch buffer on spacebar press
-        if renderer.window.is_key_down(Key::Space) {
-    
+        // ... event handling ...
+
+        // Update the angle
+        angle += rotation_speed;
+
+        // Calculate the camera's new position
+        renderer.camera.position.x = radius * angle.cos();
+        renderer.camera.position.z = radius * angle.sin();
+
+        // 5. Drawing
+        unsafe {
+            gl::UseProgram(renderer.openGLProgram);
+            
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0); // Set clear color (black in this case)
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT); // Clear the screen
+
+            // bind the specific vao for this object
+            gl::BindVertexArray(gpuData.cubeVao);
         }
 
-        // render the image to the screen
-        RenderToScreen(&mut renderer);
-    }   
+        // create all of the camera matricies which dont change for each object
+        // move the camera
+        // Create a perspective projection matrix
+        let projectionMatrix = nalgebra::Perspective3::new(
+            renderer.camera.aspectRatio, 
+            renderer.camera.fov, 
+            renderer.camera.nearPlane, 
+            renderer.camera.farPlane
+        ).to_homogeneous();
+
+        // Create a view matrix
+        let eye = Point3::new(renderer.camera.position.x, renderer.camera.position.y, renderer.camera.position.z);
+        let target = Point3::new(renderer.camera.target.x, renderer.camera.target.y, renderer.camera.target.z);
+        let viewMatrix = nalgebra::Isometry3::look_at_rh(&eye, &target, &-Vector3::y()).to_homogeneous();
+        
+        // Create a model matrix (for translation) once per object, this is where i put its actual world location eg (30, 20, -12)
+        let modelMatrix = nalgebra::Translation3::new(
+            0.0,
+            0.0,
+            0.0
+        ).to_homogeneous();
+
+        
+        unsafe {
+            // upload these to the gpu
+            gl::UniformMatrix4fv(renderer.viewMatrixLocation, 1, gl::FALSE, viewMatrix.as_ptr());
+            gl::UniformMatrix4fv(renderer.projectionMatrixLocation, 1, gl::FALSE, projectionMatrix.as_ptr());
+            gl::UniformMatrix4fv(renderer.modelMatrixLocation, 1, gl::FALSE, modelMatrix.as_ptr()); 
+            
+            // draw
+            gl::DrawElements(
+                gl::TRIANGLES,        // Mode
+                36,                   // Count of indices
+                gl::UNSIGNED_SHORT,   // Type of the indices
+                std::ptr::null()      // Offset to the EBO
+            );
+        }
+            
+
+        renderer.window.swap_buffers();
+        renderer.glfw.poll_events();
+
+
+    }
+
+
     let totalWindowDuration_ms = windowStartTime.elapsed().as_millis();
     let AvgFPS: f32 = frameNumber as f32 / (totalWindowDuration_ms as f32 / 1000.0);
     println!("Total Window Time (ms): {:?}", totalWindowDuration_ms);
