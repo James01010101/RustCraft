043bf8dc1a3cc3889d612cabf5aeb8a1afd8190f
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-30 14:26:59 +1030

    moved chunk functions into impl

--- src/Chunk.rs
+++ src/Chunk.rs
@@ -1,14 +1,6 @@
 use crate::Objects::{Block};
 use crate::Settings::*;
 
-// this is a tree which will store my spacial partitioned data for all of the objects.
-// the purpose of this is to pass to the gpu so that when it is calculating rays it only needs to check around its current location
-// and it doesnt need to check every object in the world
-// TODO: #21 implement the spatial partitioning struct and calculations
-struct PartitionedObjects {
-
-}
-
 
 // This will store all of the blocks and objects within a chunk
 pub struct Chunk {
@@ -19,30 +11,56 @@ pub struct Chunk {
     // other objects like mobs that dont fit into a single block
     //pub chunkObjects: Vec
 
-    // Chunk ID (eg. posx / chuckSizex) is the chunk
+    // Chunk ID eg. floor(posx / chuckSizex) is the chunk
     pub chunkIDX: i32,
     pub chunkIDY: i32,
 
 }
 
 
-// TODO: #20 Implement Creating chunks if they havent been created before 
-// if i havent created this chunk before then i create it, by creating a new chunk object and filling it with all the data it needs
-pub fn CreateChunk() {
 
+impl Chunk {
+    pub fn new(IDx: i32, IDy: i32) -> Chunk {
+        Chunk {
+            chunkBlocks: [[[None; chunkSizeZ]; chunkSizeY]; chunkSizeX],
+            chunkIDX: IDx,
+            chunkIDY: IDy,
+        }
+    }
+
+    
+    // TODO: #19 Implement loading chunks from file
+    // if this chunk has beenc created before then i create a Chunk obj, and fill it from wherever
+    pub fn LoadChunk(loadX: i32, loadY: i32, unloadX: i32, unloadY: i32) {
+        // params are of the x and y id of the chunk i want to load and the chunk i want to unload 
+    
+        // first check if the chunk has been created before
+
+    }
+
+
+    // TODO: #20 Implement Creating chunks if they havent been created before 
+    // if i havent created this chunk before then i create it, by creating a new chunk object and filling it with all the data it needs
+    pub fn CreateChunk() {
+
+    }
+
+
+
+
+    // TODO: #23 Implement saving chunks back to a file
+    // Once a chunk has been loaded and is in play, and then goes out of range it is unloaded and saved back to a file at certain time periods
+    pub fn SaveChunk() {
+
+    }
 }
 
 
-// TODO: #19 Implement loading chunks from file
-// if this chunk has beenc created before then i create a Chunk obj, and fill it from wherever
-pub fn LoadChunk() {
+// takes the world position and gives you the chunk id of the chunk that position is in
+pub fn GetChunkId(posX: i32, posY: i32) -> [i32; 2]{
 
-    // first check if the chunk has been created before
+    let chunkX: i32 = posX / chunkSizeX as i32;
+    let chunkY: i32 = posY / chunkSizeY as i32;
 
-}
-
-// TODO: #23 Implement saving chunks back to a file
-// Once a chunk has been loaded and is in play, and then goes out of range it is unloaded and saved back to a file at certain time periods
-pub fn SaveChunk() {
-
+    return [chunkX, chunkY];
 }
