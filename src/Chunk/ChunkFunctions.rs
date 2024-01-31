0fcbbce4c2ffb683513543e512e6fbafefb66bda
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-31 23:56:28 +1030

    working on load chunks function, getting the temp chunk vector

--- src/Chunk/ChunkFunctions.rs
+++ src/Chunk/ChunkFunctions.rs
@@ -10,11 +10,18 @@ impl super::Chunk {
     // TODO: #19 Implement loading chunks from file
     // if this chunk has beenc created before then i create a Chunk obj, and fill it from wherever
     pub fn LoadChunk(&mut self) {
+
+        // create the temp chunk Vector, which creates all blocks
+        let mut tempChunkVec: Vec<Vec<Vec<Block>>> = self.CreateTempChunkVector();
+
+        // fill the temp vector with data
         // first check if the chunk has been created before if so load it
         
         // else create a new one
+        
 
-        // fill the chunkBlocks hashmap
+        // fill the chunkBlocks hashmap from the temp vector
+        self.FillChunksHashMap(tempChunkVec);
 
     }
 
@@ -23,16 +30,23 @@ impl super::Chunk {
     // Once a chunk has been loaded and is in play, and then goes out of range it is unloaded and saved back to a file at certain time periods
     pub fn SaveChunkToFile(&mut self) {
         // save the chunk to a file then free it
-        println!("Saving Chunk to File: ({}, {})", self.chunkIDx, self.chunkIDy);
+        println!("Saving Chunk to File: ({}, {})", self.chunkIDx, self.chunkIDz);
     }
+
+
+    // TODO: #52 convert the temp chunks vector into the hashmap
+    pub fn FillChunksHashMap(&mut self, tempChunkVec: Vec<Vec<Vec<Block>>>) {
+
+    }
+
 }
 
 
 // takes the world position and gives you the chunk id of the chunk that position is in
-pub fn GetChunkId(posX: i32, posY: i32) -> [i32; 2]{
+pub fn GetChunkId(posX: i32, posZ: i32) -> [i32; 2]{
 
     let chunkX: i32 = posX / chunkSizeX as i32;
-    let chunkY: i32 = posY / chunkSizeY as i32;
+    let chunkZ: i32 = posZ / chunkSizeZ as i32;
 
-    return [chunkX, chunkY];
+    return [chunkX, chunkZ];
 }
