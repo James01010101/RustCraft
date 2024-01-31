8b7c33719cbe1a1d7425899ac00db47e56311d54
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-01-31 15:20:11 +1030

    setting up loading and unloading functions

--- src/Chunk/ChunkFunctions.rs
+++ src/Chunk/ChunkFunctions.rs
@@ -1,5 +1,5 @@
 
-use crate::Objects::*;
+use crate::Block::*;
 use crate::Settings::*;
 
 
@@ -9,18 +9,21 @@ impl super::Chunk {
     
     // TODO: #19 Implement loading chunks from file
     // if this chunk has beenc created before then i create a Chunk obj, and fill it from wherever
-    pub fn LoadChunk(loadX: i32, loadY: i32, unloadX: i32, unloadY: i32) {
-        // params are of the x and y id of the chunk i want to load and the chunk i want to unload 
-    
-        // first check if the chunk has been created before
+    pub fn LoadChunk(&mut self) {
+        // first check if the chunk has been created before if so load it
+        
+        // else create a new one
+
+        // fill the chunkBlocks hashmap
 
     }
 
 
     // TODO: #23 Implement saving chunks back to a file
     // Once a chunk has been loaded and is in play, and then goes out of range it is unloaded and saved back to a file at certain time periods
-    pub fn SaveChunk() {
-
+    pub fn SaveChunkToFile(&mut self) {
+        // save the chunk to a file then free it
+        println!("Saving Chunk to File: ({}, {})", self.chunkIDx, self.chunkIDy);
     }
 }
 
