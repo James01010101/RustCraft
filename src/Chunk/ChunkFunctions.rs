edd8b779c9a3038505e1cae29a8cb326261a07d7
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-02-01 13:04:21 +1030

    created the fill hashmap function from the vector, fixes #52.

--- src/Chunk/ChunkFunctions.rs
+++ src/Chunk/ChunkFunctions.rs
@@ -1,4 +1,8 @@
 
+use std::collections::HashSet;
+
+use gl::TEXTURE_COMPARE_FUNC;
+
 use crate::Block::*;
 use crate::Settings::*;
 
@@ -7,18 +11,23 @@ use crate::Settings::*;
 
 impl super::Chunk {
     
-    // TODO: #19 Implement loading chunks from file
+    
     // if this chunk has beenc created before then i create a Chunk obj, and fill it from wherever
-    pub fn LoadChunk(&mut self) {
+    pub fn LoadChunk(&mut self, createdChunks: &HashSet<(i32, i32)>) {
 
         // create the temp chunk Vector, which creates all blocks
         let mut tempChunkVec: Vec<Vec<Vec<Block>>> = self.CreateTempChunkVector();
 
         // fill the temp vector with data
         // first check if the chunk has been created before if so load it
-        
-        // else create a new one
-        
+        if createdChunks.contains(&(self.chunkIDx, self.chunkIDz)) {
+            // has been created before so load from file
+            self.ReadChunkFromFile(&mut tempChunkVec);
+
+        } else {
+            // else create a new one
+            self.GenerateChunk(&mut tempChunkVec);
+        }
 
         // fill the chunkBlocks hashmap from the temp vector
         self.FillChunksHashMap(tempChunkVec);
@@ -33,20 +42,55 @@ impl super::Chunk {
         println!("Saving Chunk to File: ({}, {})", self.chunkIDx, self.chunkIDz);
     }
 
+    // TODO: #19 Implement loading chunks from file
+    pub fn ReadChunkFromFile(&mut self, tempChunkVec: &mut Vec<Vec<Vec<Block>>>) {
+        // read the chunk from a file and fill the temp vector with the data
+        println!("Reading Chunk from File: ({}, {})", self.chunkIDx, self.chunkIDz);
+
+
+
+
+    }
+
 
     // TODO: #52 convert the temp chunks vector into the hashmap
     pub fn FillChunksHashMap(&mut self, tempChunkVec: Vec<Vec<Vec<Block>>>) {
 
+        // loop through the temp vector and fill the hashmap
+        for x in 0..chunkSizeX {
+            for y in 0..chunkSizeY {
+                for z in 0..chunkSizeZ {
+                    // if the block is not air then add it to the hashmap
+                    if tempChunkVec[x][y][z].blockType != BlockType::Air {
+                        self.chunkBlocks.insert(
+                            (tempChunkVec[x][y][z].position.x, tempChunkVec[x][y][z].position.y, tempChunkVec[x][y][z].position.z), 
+                            tempChunkVec[x][y][z]
+                        );
+                        self.aliveBlocks += 1;
+                    }
+                }
+            }
+        }
     }
 
 }
 
 
 // takes the world position and gives you the chunk id of the chunk that position is in
-pub fn GetChunkId(posX: i32, posZ: i32) -> [i32; 2]{
+pub fn GetChunkId(posX: i32, posZ: i32) -> (i32, i32) {
 
     let chunkX: i32 = posX / chunkSizeX as i32;
     let chunkZ: i32 = posZ / chunkSizeZ as i32;
 
-    return [chunkX, chunkZ];
+    return (chunkX, chunkZ);
+}
+
+
+// given a chunk x and z id and a block position relative to the origin of the chunk return the world coordinate of the block
+pub fn GetBlockPos(blockIDx: i32, blockIDz: i32, relBlockX: i32, relBlockY: i16, relBlockZ: i32) -> (i32, i16, i32) {
+    let worldX: i32 = (blockIDx * chunkSizeX as i32) + relBlockX;
+    let worldY: i16 = relBlockY;
+    let worldZ: i32 = (blockIDz * chunkSizeZ as i32) + relBlockZ;
+
+    return (worldX, worldY, worldZ);
 }
