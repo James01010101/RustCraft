2f0275e9f5c0c09874f4b4d528f176b9931834f7
James01010101 <104541920+James01010101@users.noreply.github.com>
2024-02-02 16:12:11 +1030

    changed chunk size back to normal

--- src/Settings.rs
+++ src/Settings.rs
@@ -4,9 +4,9 @@
 pub const worldName: &str = "James's World";
 
 // Chunk Size
-pub const chunkSizeX: usize = 8; //32;
-pub const chunkSizeY: usize = 16; //256; // half under, half above, Y is up down
-pub const chunkSizeZ: usize = 8; //32; 
+pub const chunkSizeX: usize = 32; //32;
+pub const chunkSizeY: usize = 256; //256; // half under, half above, Y is up down
+pub const chunkSizeZ: usize = 32; //32; 
 
 // TODO: #24 work out how to offert indexess so z=0 is water level and negative is under
 pub const chunkOffestZ: usize = chunkSizeZ / 2; // this is what ill take away when indexing so that z=0 is water and under is negative
