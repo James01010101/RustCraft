a8be639e09ba396d37edfcd7ee60d8da72365f3f
James01010101 <jamescoldwell73@gmail.com>
2024-02-08 15:17:30 +1030

    stores wgpu buffers now

--- src/GPUData.rs
+++ src/GPUData.rs
@@ -3,30 +3,43 @@ use crate::Renderer::*;
 use crate::World::*;
 
 use nalgebra::{Point3, Vector3};
-use glfw::Context;
+use wgpu::core::device::queue;
+use wgpu::{
+    Device,
+    Queue,
+    BufferUsages,
+    Buffer,
+    util::{BufferInitDescriptor, DeviceExt},
+    
+};
 
+use bytemuck;
 
 pub struct GPUData {
-    pub cubeVao: u32,
-    pub cubeVbo: u32,
-    pub cubeEbo: u32,
-    pub cubeInstanceVbo: u32,
-    pub cubeColoursVbo: u32,
-
     pub instancesUsed: u32, // how many of the instances am i actually using this frame
 
     pub cubeVertices: Vec<i32>,
-    pub cubeTrisIndices: Vec<u16>, 
-    pub cubeInstanceModelMatricies: [[[f32; 4]; 4]; maxBlocksRendered],
+    pub cubeIndices: Vec<u16>, 
 
+    pub cubeInstanceModelMatricies: [[[f32; 4]; 4]; maxBlocksRendered],
     pub cubeColours: [[f32; 4]; maxBlocksRendered], // temporary for now until i use textures
 
+    pub vertex_buf: wgpu::Buffer,
+    pub index_buf: wgpu::Buffer,
+    pub instance_buf: wgpu::Buffer,
+    pub colour_buf: wgpu::Buffer,
+
+    pub instance_staging_buf: wgpu::Buffer,
+    pub colour_staging_buf: wgpu::Buffer,
+
+    pub instances_modified: bool,
+    
 
 }
 
 
 impl GPUData {
-    pub fn new () -> GPUData {
+    pub fn new (device: &Device) -> GPUData {
         // cube vertices (assume starts at (0,0,0))
         let cubeVertices: Vec<i32> = vec![
             0, 0, 0, // Bottom Front Left
@@ -41,7 +54,7 @@ impl GPUData {
         ];
 
         // this is the indexes into the cubeVertices array, so it knows what vertices to use for what triangles
-        let cubeTrisIndices: Vec<u16> = vec![
+        let cubeIndices: Vec<u16> = vec![
             // Front face
             0, 1, 5, 0, 5, 4,
             // Back face
@@ -60,172 +73,75 @@ impl GPUData {
         let cubeInstanceModelMatricies: [[[f32; 4]; 4]; maxBlocksRendered] = [[[0.0; 4]; 4]; maxBlocksRendered];
 
         let cubeColours: [[f32; 4]; maxBlocksRendered] = [[0.0; 4]; maxBlocksRendered];
+        
+
+        // create the buffers for this data
+        // now create the vertex buffer for the gpu
+        let vertex_buf = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(&cubeVertices),
+            usage: BufferUsages::VERTEX,
+        });
+
+        // now make the index buffer for the gpu
+        let index_buf = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(&cubeIndices),
+            usage: BufferUsages::INDEX,
+        });
 
 
-        // Create a VAO (basically how the memory is layed out)
-        let mut cubeVao: u32 = 0;
-        unsafe {
-            gl::GenVertexArrays(1, &mut cubeVao);
-            gl::BindVertexArray(cubeVao); 
-        }
+        let instance_buf: wgpu::Buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Instance Buffer"),
+            contents: bytemuck::cast_slice(&cubeInstanceModelMatricies),
+            usage: BufferUsages::VERTEX | BufferUsages::COPY_DST,
+        });
 
-        // Create a VBO (Stores the verticies data)
-        let mut cubeVbo: u32 = 0;
-        let vboBufferSize: usize = cubeVertices.len() * std::mem::size_of::<i32>();
-        unsafe {
-            gl::GenBuffers(1, &mut cubeVbo);
-            gl::BindBuffer(gl::ARRAY_BUFFER, cubeVbo);
-            gl::BufferData(
-                gl::ARRAY_BUFFER, 
-                vboBufferSize as gl::types::GLsizeiptr,
-                cubeVertices.as_ptr() as *const gl::types::GLvoid, 
-                gl::STATIC_DRAW
-            );
-
-            // Set the vertex attributes pointers (this is for the vao but do this after vbo is bound, because it connects to the vbo)
-            let stride = 3 * std::mem::size_of::<f32>() as gl::types::GLsizei;
-            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, std::ptr::null());
-            gl::EnableVertexAttribArray(0);
-        }
-
-        // create an Element Buffer Object (to store the indexes array which points to each vertex in the verticies array)
-        let mut cubeEbo: u32 = 0;
-        let eboBufferSize: usize = cubeTrisIndices.len() * std::mem::size_of::<u16>();
-        unsafe {
-            gl::GenBuffers(1, &mut cubeEbo);
-            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, cubeEbo);
-            
-            gl::BufferData(
-                gl::ELEMENT_ARRAY_BUFFER, 
-                eboBufferSize as gl::types::GLsizeiptr, 
-                cubeTrisIndices.as_ptr() as *const gl::types::GLvoid, 
-                gl::STATIC_DRAW
-            );
-        }
-
-        // VBO for instance model matrices
-        let mut cubeInstanceVbo: u32 = 0;
-        unsafe {
-            gl::GenBuffers(1, &mut cubeInstanceVbo);
-            gl::BindBuffer(gl::ARRAY_BUFFER, cubeInstanceVbo);
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                (maxBlocksRendered * std::mem::size_of::<[[f32; 4]; 4]>()) as isize,
-                cubeInstanceModelMatricies.as_ptr() as *const gl::types::GLvoid,
-                gl::DYNAMIC_DRAW,
-            );
-
-            // Set attribute pointers for instance model matrices
-            let stride: i32 = std::mem::size_of::<[[f32; 4]; 4]>() as i32;
-            let pointerSize: u32 = std::mem::size_of::<[f32; 4]>() as u32;
-            for i in 0..4 as u32 {
-                gl::EnableVertexAttribArray(1 + i);
-                gl::VertexAttribPointer(
-                    1 + i,
-                    4,
-                    gl::FLOAT,
-                    gl::FALSE,
-                    stride,
-                    (i * pointerSize) as *const gl::types::GLvoid,
-                );
-                gl::VertexAttribDivisor(1 + i, 1); // Tell OpenGL this is instanced data
-            }
-        }
-
-        let mut cubeColoursVbo: u32 = 0;
-        unsafe {
-            gl::GenBuffers(1, &mut cubeColoursVbo);
-            gl::BindBuffer(gl::ARRAY_BUFFER, cubeColoursVbo);
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                (maxBlocksRendered * std::mem::size_of::<[f32; 4]>()) as isize,
-                cubeColours.as_ptr() as *const gl::types::GLvoid,
-                gl::STATIC_DRAW,
-            );
+        let colour_buf: wgpu::Buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Colour Buffer"),
+            contents: bytemuck::cast_slice(&cubeColours),
+            usage: BufferUsages::VERTEX | BufferUsages::COPY_DST,
+        });
 
 
-            gl::EnableVertexAttribArray(5);
-            gl::VertexAttribPointer(
-                5,
-                4,
-                gl::FLOAT,
-                gl::FALSE,
-                0,
-                std::ptr::null(),
-            );
-            gl::VertexAttribDivisor(5, 1); // Tell OpenGL this is instanced data
-        }
+        // use these staging buffers so that i can copy them to the gpu from the cpu, which takes along time, async
+        // then once the buffers are ready i copy them to the actual buffers on the gpu to be used
+        let instance_staging_buf: wgpu::Buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Instance Staging Buffer"),
+            contents: bytemuck::cast_slice(&cubeInstanceModelMatricies),
+            usage: BufferUsages::COPY_SRC | BufferUsages::COPY_DST,
+        });
 
+        let colour_staging_buf: wgpu::Buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Colour Staging Buffer"),
+            contents: bytemuck::cast_slice(&cubeColours),
+            usage: BufferUsages::COPY_SRC | BufferUsages::COPY_DST,
+        });
 
-        GPUData {
-            cubeVao,
-            cubeVbo,
-            cubeEbo,
-            cubeInstanceVbo,
-            cubeColoursVbo,
-
+        Self {
             instancesUsed: 0,
-
             cubeVertices,
-            cubeTrisIndices, 
-            cubeInstanceModelMatricies,
+            cubeIndices,
 
+            cubeInstanceModelMatricies,
             cubeColours,
 
-        }
+            vertex_buf,
+            index_buf,
+            instance_buf,
+            colour_buf,
 
+            instance_staging_buf,
+            colour_staging_buf,
+
+            instances_modified: false,
+
+            
+        }
     }
-
-
-    pub fn RenderFrame(&self, renderer: &mut Renderer) {
-
-        // clean screen
-        unsafe {
-            gl::UseProgram(renderer.openGLProgram);
-            
-            gl::ClearColor(0.0, 0.0, 0.0, 1.0); // Set clear color (black in this case)
-            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT); // Clear the screen
-
-            // bind the specific vao for this object
-            gl::BindVertexArray(self.cubeVao);
-        }
-
-        // Create a perspective projection matrix
-        let projectionMatrix = nalgebra::Perspective3::new(
-            renderer.camera.aspectRatio, 
-            renderer.camera.fov, 
-            renderer.camera.nearPlane, 
-            renderer.camera.farPlane
-        ).to_homogeneous();
-
-        // Create a view matrix
-        let eye = Point3::new(renderer.camera.position.x, renderer.camera.position.y, renderer.camera.position.z);
-        let target = Point3::new(renderer.camera.target.x, renderer.camera.target.y, renderer.camera.target.z);
-        let viewMatrix = nalgebra::Isometry3::look_at_rh(&eye, &target, &Vector3::y()).to_homogeneous();
-        
-       
-        unsafe {
-            // upload these to the gpu
-            gl::UniformMatrix4fv(renderer.viewMatrixLocation, 1, gl::FALSE, viewMatrix.as_ptr());
-            gl::UniformMatrix4fv(renderer.projectionMatrixLocation, 1, gl::FALSE, projectionMatrix.as_ptr());
-            
-
-            gl::DrawElementsInstanced(
-                gl::TRIANGLES,
-                self.cubeTrisIndices.len() as i32, // Assuming cube_indices is defined
-                gl::UNSIGNED_SHORT,
-                std::ptr::null(),
-                self.instancesUsed as i32,
-            );
-        }
-            
-
-        renderer.window.swap_buffers();
-    }
-
 
     // TODO: #57 correctly load the chunks of blocks onto the gpu
-    pub fn UpdateCubeInstances(&mut self, world: &mut World) {
+    pub fn UpdateCubeInstances(&mut self, world: &mut World, queue: &Queue) {
 
         self.instancesUsed = world.testBlocks.len() as u32;
 
@@ -237,28 +153,9 @@ impl GPUData {
             self.cubeColours[i] = world.testBlocks[i].blockType.BlockColour();
         }
 
-        // update the data on the gpu
-        unsafe {
-            // model matrix
-            gl::BindBuffer(gl::ARRAY_BUFFER, self.cubeInstanceVbo);
-            gl::BufferSubData(
-                gl::ARRAY_BUFFER,
-                0,
-                (self.instancesUsed as usize * std::mem::size_of::<[[f32; 4]; 4]>()) as isize,
-                self.cubeInstanceModelMatricies.as_ptr() as *const gl::types::GLvoid,
-            );
-
-            // colour
-            gl::BindBuffer(gl::ARRAY_BUFFER, self.cubeColoursVbo);
-            gl::BufferSubData(
-                gl::ARRAY_BUFFER,
-                0,
-                (self.instancesUsed as usize * std::mem::size_of::<[f32; 4]>()) as isize,
-                self.cubeColours.as_ptr() as *const gl::types::GLvoid,
-            );
-        }
-
+        // update the staging gpu buffers and set the flag that this data has changed
+        let updatedInstances = queue.write_buffer(&self.instance_staging_buf, 0, bytemuck::cast_slice(&self.cubeInstanceModelMatricies));
+        let updatedColours = queue.write_buffer(&self.colour_staging_buf, 0, bytemuck::cast_slice(&self.cubeColours));
+        self.instances_modified = true;
     }
-
-
 }
