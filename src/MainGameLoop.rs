b1a1c699bb105577abeafec0678cec4a5162f65b
James01010101 <jamescoldwell73@gmail.com>
2024-02-08 15:17:20 +1030

    main game loop uses wgpu

--- src/MainGameLoop.rs
+++ src/MainGameLoop.rs
@@ -8,18 +8,34 @@ use crate::GPUData::GPUData;
 use crate::Block::*;
 use crate::Chunk::*;
 use crate::WindowWrapper::*;
-
-
-extern crate gl;
-extern crate glfw;
-
-use glfw::{Context, Key, Action};
+use crate::Camera::*;
 
 use std::time::Instant;
 use std::mem;
+use async_std::task;
 
 use nalgebra::{Point3, Vector3};
 
+use wgpu::{
+    Surface,
+    Instance,
+    Adapter,
+    Device,
+    Queue,
+    ShaderModule,
+    PipelineLayout,
+    RenderPipeline,
+    SurfaceConfiguration,
+    util::{DeviceExt, BufferInitDescriptor},
+    BufferUsages,
+};
+
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::{WindowBuilder, Window},
+    dpi::LogicalSize,
+};
 
 pub fn RunMainGameLoop() {
 
@@ -32,39 +48,36 @@ pub fn RunMainGameLoop() {
     // Create the window wrapper
     let mut windowWrapper: WindowWrapper = WindowWrapper::new("RustCraft", screenWidth as u32, screenHeight as u32);
 
-
+    let mut camera: Camera = Camera::new(
+        screenFOV as f32,
+        screenWidth as u32,
+        screenHeight as u32
+    );
 
     // create Renderer and window
-    let mut renderer: Renderer = Renderer::new(screenWidth as u32, screenHeight as u32, screenFOV);
-
-    // create my kernels objects which will compile all my kernels
-    //let kernels: Kernels = CreateKernels(&renderer);
+    let mut renderer: Renderer = task::block_on(Renderer::new(&windowWrapper, &camera));
 
 
     // create MY file system struct
     let mut fileSystem: FileSystem = FileSystem::new();
-
-    // check the filesystem has folders and structure i expect
     fileSystem.CheckFileSystem();
-
-
 
     // create my world
     let mut world: World = World::new();
-
     // temp, add some blocks for testing
     world.LoadCreatedChunksFile(&mut fileSystem);
     world.AddTestBlocks();
     world.AddTestChunks(&mut fileSystem);
+
+
+    // create the gpudata buffers
+    let mut gpuData: GPUData = GPUData::new(&renderer.device);
+    gpuData.UpdateCubeInstances(&mut world, &renderer.queue);
     
 
 
 
-    // create the gpudata (vao, vbo, ebo)
-    let mut gpuData: GPUData = GPUData::new();
-
-    // update the instances buffer with the blocks model matricies
-    gpuData.UpdateCubeInstances(&mut world);
+    
     
     
     let mut angle: f32 = 0.0; // Current angle of rotation
@@ -76,6 +89,142 @@ pub fn RunMainGameLoop() {
     // stats before starting
     let mut frameNumber: u64 = 0;
     let windowStartTime: Instant = Instant::now();
+
+    // event loop
+    windowWrapper.eventLoop
+        .run(move |event, target| {
+
+
+            // check if the event is a window event, if it use get the event from inside the window event
+            if let Event::WindowEvent {
+                window_id: _, // ignore this variable
+                event,
+            } = event
+            {
+                match event {
+                    WindowEvent::Resized(new_size) => {
+
+                        println!("Surface resize {new_size:?}");
+
+                        renderer.surfaceConfig.width = new_size.width.max(1);
+                        renderer.surfaceConfig.height = new_size.height.max(1);
+                        renderer.surface.configure(&renderer.device, &renderer.surfaceConfig);
+                        
+                        // updates the porjection matrix, this doesnt exist yet
+                        /*
+                        let mx_total = renderer.generate_matrix(renderer.surfaceConfig.width as f32 / renderer.surfaceConfig.height as f32);
+                        let mx_ref: &[f32; 16] = mx_total.as_ref();
+                        renderer.queue.write_buffer(&renderer.uniform_buf, 0, bytemuck::cast_slice(mx_ref));
+                        */
+
+                        // so it always generates a new frame
+                        windowWrapper.window.request_redraw();
+
+
+                    }
+                    WindowEvent::RedrawRequested => {
+
+                        // move the camera
+                        // rotate the camera for testing
+                        angle += rotation_speed;
+                        camera.position.x = radius * angle.cos();
+                        camera.position.z = radius * angle.sin();
+
+                        // Calculate the new view and projection matrices
+                        let vertUniforms: VertexUniforms = VertexUniforms {
+                            view: camera.calculate_view_matrix().into(),
+                            projection: camera.calculate_projection_matrix().into(),
+                        };
+
+
+                        // Create a temporary buffer with the new data for the uniform buffer
+                        let staging_buffer = renderer.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                            label: Some("Staging uniform Buffer"),
+                            contents: bytemuck::bytes_of(&vertUniforms),
+                            usage: wgpu::BufferUsages::COPY_SRC,
+                        });
+
+
+
+
+                        // calculate the frams
+                        let frame = renderer.surface
+                            .get_current_texture()
+                            .expect("Failed to acquire next swap chain texture");
+                        let view = frame
+                            .texture
+                            .create_view(&wgpu::TextureViewDescriptor::default());
+                        let mut encoder =
+                            renderer.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                                label: None,
+                            });
+                        
+                        // update the uniform buffer with the new camera position matricies
+                        encoder.copy_buffer_to_buffer(&staging_buffer, 0, &renderer.uniform_buffer, 0, staging_buffer.size());
+                        
+
+                        // if my instances have changed then i update the instance buffer with its staging buffer
+                        if gpuData.instances_modified {
+                            encoder.copy_buffer_to_buffer(&gpuData.instance_staging_buf, 0, &gpuData.instance_buf, 0, gpuData.instance_staging_buf.size());
+                            encoder.copy_buffer_to_buffer(&gpuData.colour_staging_buf, 0, &gpuData.colour_buf, 0, gpuData.colour_staging_buf.size());
+                            gpuData.instances_modified = false;
+                        }
+                        let depth_texture_view = &renderer.depth_texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+                        // set all of the commands i will use in the render pass
+                        {
+                            let mut rpass =
+                                encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                                    label: None,
+                                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                        view: &view,
+                                        resolve_target: None,
+                                        ops: wgpu::Operations {
+                                            load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                                            store: wgpu::StoreOp::Store,
+                                        },
+                                    })],
+
+
+                                    depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                                        view: depth_texture_view,
+                                        depth_ops: Some(wgpu::Operations {
+                                            load: wgpu::LoadOp::Clear(1.0),
+                                            store: wgpu::StoreOp::Store,
+                                        }),
+                                        stencil_ops: None,
+                                    }),
+
+                                    timestamp_writes: None,
+                                    occlusion_query_set: None,
+                                });
+
+                            // Set the vertex and index buffers here
+                            rpass.set_vertex_buffer(0, gpuData.vertex_buf.slice(..));
+                            rpass.set_index_buffer(gpuData.index_buf.slice(..), wgpu::IndexFormat::Uint16);
+                            rpass.set_bind_group(0, &renderer.bind_group, &[]);
+                            rpass.set_vertex_buffer(1, gpuData.instance_buf.slice(..));
+                            rpass.set_vertex_buffer(2, gpuData.colour_buf.slice(..));
+
+                            rpass.set_pipeline(&renderer.render_pipeline);
+                            rpass.draw_indexed(0..36, 0, 0..5);
+                        } // the render pass must go out of scope before submit and present are called
+                        // it finalises the render pass when it goes out of scope so it can be submitted to the gpu
+
+                        renderer.queue.submit(Some(encoder.finish()));
+                        frame.present();
+
+                        // so it always generates a new frame
+                        windowWrapper.window.request_redraw();
+                    }
+                    WindowEvent::CloseRequested => target.exit(),
+                    _ => {}
+                };
+            }
+        })
+        .unwrap();
+
+    /* Old Event Loop 
     while !renderer.window.should_close() {
         frameNumber += 1; // keep increasing frame number
 
@@ -87,7 +236,7 @@ pub fn RunMainGameLoop() {
         renderer.camera.position.z = radius * angle.sin();
 
 
-        // TODO: #35 deal with events
+        
         renderer.glfw.poll_events();
         for (_, event) in glfw::flush_messages(&renderer.events) {
             match event {
@@ -147,9 +296,8 @@ pub fn RunMainGameLoop() {
         // Render the frame
         gpuData.RenderFrame(&mut renderer);
         
-        
-
     }
+    */
 
     CleanUp(&mut world, &mut fileSystem);
 
