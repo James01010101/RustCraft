83612697d27019e5f52de8d185db67ee43ad78af
James01010101 <jamescoldwell73@gmail.com>
2024-02-08 15:16:23 +1030

    removed kernels mod

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,6 @@
 pub mod MainGameLoop; // where i create the window and renderer and the main loop
 pub mod Renderer;
 pub mod Camera; // anything to do with camera
-pub mod Kernels;    // where i store the compiled kernels
 pub mod Block; // where i create my basic objects like spheres and squares
 pub mod World; // this is where all of the objects in the world are stored
 pub mod Settings; // this is a const settings file for things like chunk size, ray bounces ect.\
